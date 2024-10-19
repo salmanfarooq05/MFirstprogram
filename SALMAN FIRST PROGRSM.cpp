3de8f552f079868c8a2c863b15027b44e26e77fe
salmanfarooq05 <nicesalman05@gmail.com>
2024-10-19 15:38:15 +0500

    Update SALMAN FIRST PROGRSM.cpp

--- SALMAN FIRST PROGRSM.cpp
+++ SALMAN FIRST PROGRSM.cpp
@@ -2,7 +2,28 @@
 using namespace std;
 int main()
 {
-	cout << "WELCOME TO GITHUB" << endl;
-	cout << "Welcome to UCP"<<endl;
+	#include <iostream>
+using namespace std;
+
+int main() {
+    int number, range;
+
+    // Ask user for the number and the range
+    cout << "Enter a number to generate its multiplication table: ";
+    cin >> number;
+
+    cout << "Enter the range for the multiplication table: ";
+    cin >> range;
+
+    cout << "Multiplication table of " << number << ":\n";
+
+    for (int i = 1; i <= range; i++) {
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
+
+    return 0;
+}
+
+	
 	return 0;
 }

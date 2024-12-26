ea95cfb69e7e6821b844d11ff94d5e293a413f59
salmanfarooq05 <nicesalman05@gmail.com>
2024-12-26 09:51:41 +0500

    Nested loop lab

---
+++ Lab 9.cpp
@@ -0,0 +1,172 @@
+#include<iostream>
+using namespace std;
+int main()
+//Task 1
+//{
+//	int n;
+//	cout << "Enter the number: ";
+//	cin >> n;
+//	for (int i = 0; i <= n;n-=1)
+//	{
+//		cout << n << " ";
+//	}
+//	return 0;
+//}
+
+//Task 2 (a)
+//{
+//	int n;
+//	cout << "Enter the number: ";
+//	cin >> n;
+//	for (int i = 0; i <= n; i++)
+//	{
+//		cout << i << " ";
+//	}
+//	return 0;
+//}
+
+//Task 2 (b)
+//{
+//	
+//	for(int i =10;i>=0; i--)
+//	{
+//		cout << i << " ";
+//		
+//	}
+//	return 0;
+//}
+//Task 3
+//{
+//	int n;
+//	int sum = 0;
+//	cout << "Enter the number: ";
+//	cin >> n;
+//	for (int i = 0; i <= n; i++)
+//	{
+//		cout << i << " ";
+//	     sum += i;
+//	}
+//	cout << "\nSum of natural numbers is: " << sum;
+//	return 0;
+//}
+
+//Task 4 (a)
+//{
+//	for (int i = 0; i<=20; i+=2)
+//	{
+//		cout <<i<<" ";
+//	}
+//	return 0;
+//}
+//Task 4 (b)
+//{
+//	for (int i = 1; i <= 20; i += 2)
+//	{
+//		cout << i << " ";
+//	}
+//	return 0;
+//}
+//Task 5
+//{
+//	int n;
+//	int fact = 1;
+//	cout << "Enter the number: ";
+//	cin >> n;
+//	while (n != 0)
+//	{
+//		fact *= n;
+//		n--;
+//	}
+//	cout << fact;
+//}
+//Task 6
+//{
+//	int n;
+//	cout << "Enter the number for multiplication table: ";
+//	cin >> n;
+//	int range;
+//	cout << "Enter the range: ";
+//	cin >> range;
+//	for (int i = 1; i <= range; i++)
+//	{
+//		cout << n << "x" << i << "=" << n * i<< endl;;
+//	}
+//}
+//Task 7
+
+//{
+//	int num1;
+//	int num2;
+//	cout << "Enter the number 1: ";
+//	cin >> num1;
+//	cout << "Enter the number 2: ";
+//	cin >> num2;
+//	for (int i = num1; i <= num2; i++)
+//	{
+//		cout << i << " ";
+//	}
+//	
+//}
+//Task 8
+//{
+//	int rows;
+//	cout << "Enter the Rows: ";
+//	cin >> rows;
+//	for (int i = 1; i <= rows; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//}
+//Task 9
+//{
+//	int rows;
+//	cout << "Enter the Rows: ";
+//	cin >> rows;
+//	for (int i = 1; i <= rows; i++)
+//	{
+//		for (int j = 1; j <= rows; j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//}
+//Task 10
+//{
+//	int rows;
+//	cout << "Enter the Rows: ";
+//	cin >> rows;
+//	for (int i = rows; i > 0; i--)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//}
+//Task 11
+//{
+//	
+//	for (int i = 1; i <= 4; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//	for (int i = 4; i > 0; i--)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//}
+
